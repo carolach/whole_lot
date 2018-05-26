5feb3c7fab795cde422e395ef35c4cb7cdc8964b
Caroline Lachance <caro.lachance@outlook.com>
2018-05-26 12:23:16 -0400

    added contact and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
   root 'static_pages#index'
+  get 'static_pages/index'
+
+  get 'static_pages/about'
+  get 'static_pages/contact'
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
